fa61f281c274171acdd1ee047623b4e724771611
Thanakorn Saengsee <dsffdf567@gmail.com>
2026-06-06 14:24:08 +0700

    Step 4: Sperage the home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:learn_flutter_68_2/screen/home.dart';
-void main() {
-
- // const app = MaterialApp(title: "หัวข้อ", home: Text("Hello World"));
-  // runApp(app);
-
-
-runApp(
-  MaterialApp(
-    title: "หัวข้อ",
-    home: Scaffold(
-    appBar: AppBar(
-      title: Text("หัวข้อ"),
-      backgroundColor: Colors.pinkAccent,
-      centerTitle: true,
-      ),
-      body: Home(),
-    ),
-  ),
-);
-}
-
 class Home extends StatelessWidget {
-  const Home ({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
