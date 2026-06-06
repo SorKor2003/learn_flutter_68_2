f7e4068330b1863cdec0cf5d7d74d882cb52bc32
Thanakorn Saengsee <dsffdf567@gmail.com>
2026-06-06 14:31:01 +0700

    Step 4.2: Sperage the home

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,8 +11,10 @@ class Home extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         padding: EdgeInsets.all(50),
         child: Text(
-          "Hello World 3",
-          style: TextStyle(fontSize: 30, color: Colors.white),
+          "Hello World 4",
+          style: TextStyle(fontSize: 30, color: Colors.white,
+          fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
