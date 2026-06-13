63defa884c1cfe40b6226c0b20b39aa40993dc23
Thanakorn Saengsee <dsffdf567@gmail.com>
2026-06-13 11:24:19 +0700

    step7: button widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,18 +5,58 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(
-          'assets/images/grok-image-d0db6409-2873-41f5-a333-21041a0f556f.jpg',
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover,
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.pinkAccent,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว TextButton");
+            },
+            child: const Text("กดปุ่ม"),
+          ),
+          const SizedBox(height: 20),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 76, 236, 44),
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว FilledButton");
+            },
+            child: const Text("กดปุ่ม"),
+          ),
+           const SizedBox(height: 20),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              foregroundColor: const Color.fromARGB(255, 33, 10, 240),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว OutlinedButton");
+            },
+            child: const Text("กดปุ่ม"),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 97, 114, 3),
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว ElevatedButton");
+            },
+            child: const Text("กดปุ่ม"),
+          ),
+        ],
       ),
     );
   }
